bbcad494619d30c0321b5a05542e0ee3a26db0ce
Alex_Mueller <mueller@ibb.uni-stuttgart.de>
2022-12-01 16:53:26 +0100

    add licenses (#6)

---
+++ LICENSE.md
@@ -0,0 +1,7 @@
+Copyright holders:
+==================
+2021-2022 Alexander Müller
+2022      Tarun Kumar Mitruka Vinod Kumar Mitruka
+
+All licenses reside at the folder LICENSES.
+Each source file here has the corresponding header for license information
--- src/iks008_vonmises_truss.cpp
+++ src/iks008_vonmises_truss.cpp
@@ -1,22 +1,5 @@
-/*
- * This file is part of the Ikarus distribution
- * (https://github.com/IkarusRepo/Ikarus). Copyright (c) 2022. The Ikarus
- * developers.
- *
- * This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
- */
+// SPDX-FileCopyrightText: 2022 The Ikarus Developers mueller@ibb.uni-stuttgart.de
+// SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include <config.h>
 
@@ -63,7 +46,7 @@ struct Truss : Ikarus::PowerBasisFE<Basis>, Ikarus::AutoDiffFE<Truss<Basis>, Bas
     localView_.bind(element);
   }
 
-private:
+ private:
   template <class Scalar>
   Scalar calculateScalarImpl(const FERequirementType &par, const Eigen::VectorX<Scalar> &dx) const {
     const auto &d      = par.getSolution(Ikarus::FESolutions::displacement);
@@ -91,7 +74,7 @@ private:
     return 0.5 * EA / sqrt(LRefsquared) * Egl * Egl;
   }
 
-private:
+ private:
   LocalView localView_;
   double EA;
 };
