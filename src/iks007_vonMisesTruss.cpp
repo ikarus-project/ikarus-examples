78cb48f0bb98a2088d8692862a4600f4f3c134ff
Tarun Mitruka <mitruka@ibb.uni-stuttgart.de>
2024-02-18 19:35:17 +0100

    fixes according to changes in observer patterns

--- src/iks007_vonMisesTruss.cpp
+++ src/iks007_vonMisesTruss.cpp
@@ -19,6 +19,7 @@
 #include <autodiff/forward/dual/dual.hpp>
 
 #include <ikarus/assembler/simpleassemblers.hh>
+#include <ikarus/controlroutines/controlinfos.hh>
 #include <ikarus/controlroutines/loadcontrol.hh>
 #include <ikarus/finiteelements/autodifffe.hh>
 #include <ikarus/finiteelements/fefactory.hh>
@@ -192,12 +193,13 @@ int main(int argc, char** argv) {
   lambdaAndDisp.setZero(Eigen::NoChange, loadSteps + 1);
   /// Create Observer which executes when control routines messages
   /// SOLUTION_CHANGED
-  auto lvkObserver = std::make_shared<Ikarus::GenericObserver<Ikarus::ControlMessages>>(
-      Ikarus::ControlMessages::SOLUTION_CHANGED, [&](int step) {
-        lambdaAndDisp(0, step) = lambda;
-        lambdaAndDisp(1, step) = d[2];
-        lambdaAndDisp(2, step) = d[3];
-      });
+  auto lvkObserver
+      = std::make_shared<Ikarus::GenericObserver<Ikarus::ControlMessages, Ikarus::ControlLoggerInformation>>(
+          Ikarus::ControlMessages::SOLUTION_CHANGED, [&](int step) {
+            lambdaAndDisp(0, step) = lambda;
+            lambdaAndDisp(1, step) = d[2];
+            lambdaAndDisp(2, step) = d[3];
+          });
 
   /// Create Observer which writes vtk files when control routines messages
   /// SOLUTION_CHANGED
